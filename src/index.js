85891478ead5bc7f27762d5179eae9b658b00da3
Greg Hurrell <greg@hurrell.net>
2016-02-18 18:43:12 -0800

    Initial commit
    Dump of https://github.com/wincent/minimal-npm-boilerplate at current
    HEAD of master (b17f8392ac109e551d0).

---
+++ src/index.js
@@ -0,0 +1,12 @@
+/**
+ * Copyright 2015-present Greg Hurrell. All rights reserved.
+ * Licensed under the terms of the MIT license.
+ *
+ * @flow
+ */
+
+'use strict';
+
+import 'babel-core/polyfill';
+
+export default function index() {}
